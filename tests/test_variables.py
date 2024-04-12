094f3bc062de75daa73d8d3c8c57d9cd2a31a7b2
Micah Sandusky <micc5725@gmail.com>
2024-04-12 10:39:09 -0600

    some TODO tests

---
+++ tests/test_variables.py
@@ -0,0 +1,12 @@
+import pytest
+
+from loomsitu.campaigns.variables import ProfileVariables
+
+
+class TestVariables:
+    # TODO: these test
+    def test_variable_attributes(self):
+        pass
+
+    def test_variable_list(self):
+        pass
